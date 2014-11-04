1f525ff95af1fd9b376022cab4db3ab15d1a4e25
David McGraw <dlmcgraw07@gmail.com>
2014-11-04 11:10:57 -0600

    add an animation so we can see a live action behind the modal controller

--- dojo-blur/XMCHomeViewController.swift
+++ dojo-blur/XMCHomeViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import QuartzCore
 
 class XMCHomeViewController: UIViewController {
     // MARK: Properties
     @IBOutlet weak var modalButton: UIButton!
     
+    // Starts off with 0.0f alpha
+    @IBOutlet weak var movingBox: UIView!
+    @IBOutlet weak var movingBoxCenterYConstraint: NSLayoutConstraint!
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        beginMovingBoxAnimation()
+    }
+    
     // MARK: IBActions
     @IBAction func closeModalController(segue: UIStoryboardSegue) {
+        stopMovingBoxAnimation();
+        
         dismissViewControllerAnimated(true, completion: nil);
+    }
+    
+    // MARK: Animation  Helpers
+    func beginMovingBoxAnimation() {
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.movingBox.alpha = 1.0
+        })
+        
+        self.view.needsUpdateConstraints()
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.Autoreverse | UIViewAnimationOptions.Repeat, animations: { () -> Void in
+            self.movingBoxCenterYConstraint.constant = -self.movingBoxCenterYConstraint.constant
+            self.view.layoutIfNeeded()
+            }, completion: { (value: Bool) in
+        })
+    }
+    
+    func stopMovingBoxAnimation() {
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.movingBox.alpha = 0.0
+        })
+        self.view.layer.removeAllAnimations()
     }
 }
 
